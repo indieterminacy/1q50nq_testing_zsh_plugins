363c59677cf97dfe01e985b9a682b20f0b8f4bd1
indieterminacy <indieterminacy@nortonspeal.com>
2016-09-24 16:21:28 +0200

    Creating repo for testing tools, featuring Python tools

---
+++ nq_testing.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/zsh
+
+# https://github.com/alfredodeza/zsh-plugins
+####################
+# oq Python
+####################
+####################
+# oq PyLint
+####################
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/pylint
